32ff83e3d2eef8035a236981597696429fa68e2f
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-21 18:33:59 -0500

    Add some more print statements and comments ^w^

--- Source/main.cpp
+++ Source/main.cpp
@@ -11,14 +11,29 @@ void print(std::string msg) {
 }
 
 void createWindow() {
-	
-	Window = glfwCreateWindow(1, 1, "Not Enough Light v1.0.0", nullptr, nullptr);
-	glfwMaximizeWindow(Window);
-	glfwMakeContextCurrent(Window);
-	gladLoadGL(glfwGetProcAddress);
+	print(
+		"\x1b[32m"
+		">> "
+		"\x1b[0;3m"
+		"Creating window..."
+		"\x1b[m"
+	);
 
+	// Create a window //
+	Window = glfwCreateWindow(1, 1, "Not Enough Light v1.0.0", nullptr, nullptr);
+	
+	// Load OpenGL function pointers //
+	// (This must be done after creating the window) //
+	glfwMakeContextCurrent(Window);
+	gladLoadGL(glfwGetProcAddress);	
+
+	// Maximize the window and pass the dimensions over to OpenGL //
+	glfwMaximizeWindow(Window);
 	glfwGetWindowSize(Window, &width, &height);
 	glViewport(0, 0, width, height);
+
+	// Minimize again to show off my fancy print statements .w. //
+	glfwMinimizeWindow(Window);
 }
 
 void tick() {
@@ -33,16 +48,23 @@ int main() {
 		"\x1b[1;34m"
 		"-----------------------\n"
 		"Not Enough Light v1.0.0\n"
-		"-----------------------\n"
+		"-----------------------"
 		"\x1b[m"
 	);
 	
 	// Initialize GLFW //
 	glfwInit();
 
-	// Create a Window to display graphics on //
+	// Create a window to display graphics on //
 	createWindow();
+
+	// Maximize window one last time before starting mainloop //
+	glfwMaximizeWindow(Window);
+
+	// Run mainloop until GLFW says we should stop //
 	while (!glfwWindowShouldClose(Window)) tick();
 
+	// Tell GLFW to clean up its mess :3c //
+	glfwTerminate();
 	return 0;
 }
