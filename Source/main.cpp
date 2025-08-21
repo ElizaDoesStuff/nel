d0cb88041e7c730055dcedd8757bbfce32db7149
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-21 14:06:29 -0500

    Print fancy header on startup :3

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream> 
+#include "../Dependencies/glad/include/glad/glad.h"
+#include "../Dependencies/glfw/include/glfw/glfw3.h"
+
+#include <iostream>
 
 int main() {
-	std::cout << "Hello, nel!" << std::endl;
+	// Print Header :3
+	std::cout << "\x1b[2;34m-----------------"            "------\x1b[m" << std::endl;
+	std::cout << "\x1b[1;34mNot Enough Light \x1b[0;2;3;34mv1.0.0\x1b[m" << std::endl;
+	std::cout << "\x1b[2;34m-----------------"            "------\x1b[m" << std::endl;
+	
 	return 0;
 }
+
