199669169db89634332a302089b5a871f5e613d3
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-21 18:23:17 -0500

    Create window + cleaned up header printing

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,12 +3,46 @@
 
 #include <iostream>
 
-int main() {
-	// Print Header :3
-	std::cout << "\x1b[2;34m-----------------"            "------\x1b[m" << std::endl;
-	std::cout << "\x1b[1;34mNot Enough Light \x1b[0;2;3;34mv1.0.0\x1b[m" << std::endl;
-	std::cout << "\x1b[2;34m-----------------"            "------\x1b[m" << std::endl;
-	
-	return 0;
+GLFWwindow* Window;
+int width, height;
+
+void print(std::string msg) {
+	std::cout << msg << std::endl;
 }
 
+void createWindow() {
+	
+	Window = glfwCreateWindow(1, 1, "Not Enough Light v1.0.0", nullptr, nullptr);
+	glfwMaximizeWindow(Window);
+	glfwMakeContextCurrent(Window);
+	gladLoadGL(glfwGetProcAddress);
+
+	glfwGetWindowSize(Window, &width, &height);
+	glViewport(0, 0, width, height);
+}
+
+void tick() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(Window);
+	glfwPollEvents();
+}
+
+int main() {
+	// Print Header :3 //
+	print(
+		"\x1b[1;34m"
+		"-----------------------\n"
+		"Not Enough Light v1.0.0\n"
+		"-----------------------\n"
+		"\x1b[m"
+	);
+	
+	// Initialize GLFW //
+	glfwInit();
+
+	// Create a Window to display graphics on //
+	createWindow();
+	while (!glfwWindowShouldClose(Window)) tick();
+
+	return 0;
+}
