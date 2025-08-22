c21b4681f75e1cb0dec29b5b2453af4194892037
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-22 04:48:32 -0500

    Update print function; Add stuff for generating geometry

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,22 +2,18 @@
 #include "../Dependencies/glfw/include/glfw/glfw3.h"
 
 #include <iostream>
+#include <string>
+
+int color = 0;
+std::string colors[3] = {"\x1b[31m", "\x1b[32m", "\x1b[34m"};
+void print(const char* message) {
+	std::cout << colors[color++ % 3] + ">> " + "\x1b[0;3m" + message + "\x1b[m" << std::endl;
+}
 
 GLFWwindow* Window;
 int width, height;
-
-void print(std::string msg) {
-	std::cout << msg << std::endl;
-}
-
 void createWindow() {
-	print(
-		"\x1b[32m"
-		">> "
-		"\x1b[0;3m"
-		"Creating window..."
-		"\x1b[m"
-	);
+	print("Creating window...");
 
 	// Create a window //
 	Window = glfwCreateWindow(1, 1, "Not Enough Light v1.0.0", nullptr, nullptr);
@@ -27,36 +23,79 @@ void createWindow() {
 	glfwMakeContextCurrent(Window);
 	gladLoadGL(glfwGetProcAddress);	
 
+
 	// Maximize the window and pass the dimensions over to OpenGL //
 	glfwMaximizeWindow(Window);
 	glfwGetWindowSize(Window, &width, &height);
 	glViewport(0, 0, width, height);
 
 	// Minimize again to show off my fancy print statements .w. //
-	glfwMinimizeWindow(Window);
+	glfwIconifyWindow(Window);
+}
+
+unsigned int VertexBuffer;
+void createVertexBuffer() {
+	print("Generating geometry...");
+	
+	// Create & bind vertex buffer //
+	glGenBuffers(1, &VertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, VertexBuffer);
+	
+	// Populate buffer with fullscreen quad //
+	const float QuadMesh[] = {
+		-1.0,  1.0,
+		 1.0,  1.0,
+		 1.0, -1.0,
+
+		 1.0, -1.0,
+		-1.0, -1.0,
+		-1.0,  1.0,
+	};
+	glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), QuadMesh, GL_STATIC_DRAW);
+
+	// Set up vertex attributes (literally just position lmao) //
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glEnableVertexAttribArray(0);
+
+	// I was too lazy to reverse the winding order in my mesh //
+	glFrontFace(GL_CW);
 }
 
 void tick() {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
 	glfwSwapBuffers(Window);
 	glfwPollEvents();
 }
 
 int main() {
 	// Print Header :3 //
-	print(
-		"\x1b[1;34m"
+	std::cout <<
+		"\x1b[1m"
 		"-----------------------\n"
 		"Not Enough Light v1.0.0\n"
 		"-----------------------"
 		"\x1b[m"
-	);
+	<< std::endl;
 	
+	///////////
+	// Setup //
+	///////////
+
 	// Initialize GLFW //
 	glfwInit();
 
 	// Create a window to display graphics on //
 	createWindow();
+
+	// Create a vertex buffer and populate it with a rectangle that covers the whole screen //
+	createVertexBuffer();
+
+	///////////////
+	// Main Loop //
+	///////////////
 
 	// Maximize window one last time before starting mainloop //
 	glfwMaximizeWindow(Window);
