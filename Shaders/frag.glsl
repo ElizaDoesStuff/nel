d1a4217cac523502a015fa10ad301667f972a11b
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-08-25 15:02:45 -0500

    Wrote some basic shaders -- bare minimum vertex shader and the beginnings of the pathtracing shader

--- Shaders/frag.glsl
+++ Shaders/frag.glsl
@@ -1,1 +1,21 @@
-#version 440 core 
+#version 330 core 
+ 
+// Defines an output variable to return the pixel's color //
+out vec4 FragColor;
+
+// Get the aspect ratio of the window from the CPU //
+uniform float uWidth;
+uniform float uHeight;
+uniform float uAspectRatio;
+
+// Also take some camera settings from the CPU //
+uniform vec3 uCameraPosition;
+uniform vec3 uCameraRotation;
+
+// Calculate the ray's origin and direction from the screen dimensions and camera settings //
+vec3 RayOrigin = uCameraPosition;
+vec3 RayDirection = vec3(0) + vec3(0) + 1 * uCameraDirection;
+
+void main() {
+	FragColor = vec4(gl_FragCoord.x / uWidth, gl_FragCoord.y / uHeight, uAspectRatio, 1.0);
+}
--- Shaders/vert.glsl
+++ Shaders/vert.glsl
@@ -1,1 +1,10 @@
-#version 440 core 
+#version 330 core 
+
+layout(location = 0) in vec2 vPosition;
+
+void main() {
+	
+	gl_Position = vec4(vPosition,1.0,1.0);
+	return;
+	
+}
